247d8d81f0f6d5f51cbf6a708c5afa65f1ded150
imdnasa <126351317+imdnasa@users.noreply.github.com>
2023-03-18 13:57:01 -0400

    Clean Up (3-18-2023)
    Removed unnecessary comments, added clarifying comments, and removed vestigial code

--- UdpSocketv2.cs
+++ UdpSocketv2.cs
@@ -22,7 +22,7 @@ using System.Threading;
 
 public class UdpSocketv2 : MonoBehaviour
 {
-
+    //Data fields needed for the Socket
     [SerializeField] string IP = "127.0.0.1"; // local host
     [SerializeField] int rxPort = 8000; // port to receive data from Python on
     [SerializeField] int txPort = 8001; // port to send data to Python on
@@ -30,18 +30,29 @@ public class UdpSocketv2 : MonoBehaviour
     // Create necessary UdpClient objects
     UdpClient client;
     IPEndPoint remoteEndPoint;
-    Thread receiveThread; // Receiving Thread
+    
+    void Awake()
+    {
+
+        //Sets up the UDP Sockets and Clients
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), txPort);
+        client = new UdpClient(rxPort);
+
+
+        Debug.Log("UDP Comms Initialised");
+
+    }
 
     void Update() 
     {
-        SendData(gameObject.transform.position.ToString());
+        SendData(gameObject.transform.position.ToString()); //SENDS POSITIONAL DATA
     }
 
     public void SendData(string message) // Use to send data to Python
     {
         try
         {
-            byte[] data = Encoding.UTF8.GetBytes(message);
+            byte[] data = Encoding.UTF8.GetBytes(message);//message is the string containing the data
             client.Send(data, data.Length, remoteEndPoint);
         }
         catch (Exception err)
@@ -49,54 +60,12 @@ public class UdpSocketv2 : MonoBehaviour
             print(err.ToString());
         }
     }
-
-    void Awake()
-    { 
-        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), txPort);
-
-        client = new UdpClient(rxPort);
-
-        
-        receiveThread = new Thread(new ThreadStart(ReceiveData));
-        receiveThread.IsBackground = true;
-        receiveThread.Start();
-        
-        Debug.Log("UDP Comms Initialised");
-
-    }
-
      
-    
-    private void ReceiveData()
-     {
-         while (true)
-         {
-             try
-             {
-                 
-                IPEndPoint anyIP = new IPEndPoint(IPAddress.Parse(IP), rxPort);
-                byte[] data = client.Receive(ref anyIP);
-
-                string text = Encoding.UTF8.GetString(data);
-                print(">> " + text);
-                //imageData = data;
-               
-             }
-             catch (Exception err)
-             {
-                 print(err.ToString());
-             }
-         }
-     }
-
-    //Prevent crashes - close clients and threads properly!
+    //Prevent crashes - close client properly!
     
     void OnDisable()
     {
-        if (receiveThread != null)
-            receiveThread.Abort();
-
         client.Close();
-    }
+    } 
     
 }
--- amir_pptk.py
+++ amir_pptk.py
@@ -25,26 +25,14 @@ data = plyfile.PlyData.read('./points.ply')['vertex']
 xyz = np.c_[data['x'], data['y'], data['z']]
 rgb = np.c_[data['red'], data['green'], data['blue']]
 n = np.c_[data['nx'], data['ny'], data['nz']]
-#n = np.zeros_like(xyz)
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 8001
-UDP_PORT_SEND = 8000
 
 v = pptk.viewer(xyz, debug=True)
-print(len(n))
-print(len(rgb))
 v.attributes(rgb / 255., 0.5 * (1 + n))
-# attr1 = pptk.rand(100)     # 100 random scalars
-# attr2 = pptk.rand(100, 3)  # 100 random RGB colors
-# attr3 = pptk.rand(100, 4)  # 100 random RGBA colors
-# attr4 = pptk.rand(1)       # 1 random scalar
-# attr5 = pptk.rand(1, 3)    # 1 random RGB color
-# attr6 = pptk.rand(1, 4)    # 1 random RGBA color
-# v.attributes(attr1, attr2, attr3, attr4, attr5, attr6)
 
 v.set(show_info = False, show_axis = False, show_grid = False, bg_color = [1,1,1,1])
-rod = 0
 
 send = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 recv = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
@@ -54,13 +42,10 @@ recv.bind((UDP_IP, UDP_PORT))
 timeout = 120
 readable, _, _ = select.select([recv], [], [], timeout)
 
-#print(v.get('eye'))
 while readable:
 
     if msvcrt.kbhit():
-        # Get the ASCII value of the key pressed
         key = ord(msvcrt.getch())
-        # If the key pressed is 'q', break out of the loop
         if key == ord('q'):
             break
     
@@ -68,6 +53,7 @@ while readable:
 
     poses = []
 
+    #Receive data from socket
     data, addr = recv.recvfrom(1024)
 
     if not data:
@@ -78,26 +64,23 @@ while readable:
     #regex to parse (x,y,z)
     pattern = "\((-?\d+\.\d+), (-?\d+\.\d+), (-?\d+\.\d+)\)"
     result = re.findall(pattern, data_str)
-    #print(result)
+
     if result: 
+
+        #Stores extracted data into poses
         x, y, z = result[0]
-        # Extract the values
-        #print(f"X: {x}, Y: {y}, Z: {z}")
-        #if(rod < 5):
-        #    print(poses)
         poses = [[float(x), float(y), float(z) + 48, 0, np.pi/4, 5]]
-        # if (rod < 10):
+        
+        #For DEBUG purposes
         print(poses)
 
+        #reorients the camera according to poses
         v.play(poses, 2 * np.arange(len(poses)), repeat=False, interp='linear')
         
+        #Stores the file path to the image as a string
         fpath = r'D:\Unity\AzureKinect\Assets\PPTK Textures\img.png'
 
-        # if (os.path.isfile(path)): #'./rec/img.png'
-        #     #print("there is file")
-        #     os.remove(path)
-        
-        # Attempt to delete previous file before savin
+        # Deletes old image because 'v.capture' doesn't overwrite
         if os.path.exists(fpath):
             try:
                 os.remove(fpath)
@@ -107,25 +90,12 @@ while readable:
         else:
             print(f"The file {fpath} does not exist.")
 
+        #Saves the new image
         with open(fpath, "w") as file:
-        
             v.capture(fpath)
             file.flush()  # flush the buffer
             print("i create image")
             time.sleep(2)
         
 
-        #time.sleep(0.1)
-        #open the image file
-        # with open('./rec/img.png', "rb") as f:
-        #     image_bytes = f.read()
-        # #Pack the length of the image bytes as a 4-byte integer
-        # length = len(image_bytes)
-        # length_bytes = struct.pack("!I", length)
-
-        # send.sendto(image_bytes, (UDP_IP, UDP_PORT_SEND))
-        #send.sendto(b"HELLO SIR", (UDP_IP, UDP_PORT_SEND))
-        
-        rod = rod + 1
-
-        readable, _, _ = select.select([recv], [], [], timeout)
+    readable, _, _ = select.select([recv], [], [], timeout)
